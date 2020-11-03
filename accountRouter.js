9edb104c57a731fd68f90d6a9753595cef1e3a0e
John Youn <john@Johns-MacBook-Pro.local>
2020-11-02 16:17:53 -0800

    mvp

--- accountRouter.js
+++ accountRouter.js
@@ -19,3 +19,65 @@ const Accounts = {
         return db('posts').where({id}).del()
     }
 }
+
+router.get('/',(req, res) => {
+    Accounts.getAll()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(`${err}`)
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Accounts.getById(req.params.id)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(`${err}`)
+    })
+})
+
+router.post('/', (req, res) => {
+    Accounts.create(req.body)
+    .then((id) => {
+        return Accounts.getById(id).first()
+    })
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(`${err}`)
+    })
+})
+
+router.put('/:id',(req, res) => {
+    Accounts.update(req.params.id, req.body)
+    .then(count => {
+        if (!count) {
+            res.json({message: 'no account with that id'})
+        } else {
+            return Accounts.getById(req.params.id).first()
+        }
+    })
+    .then(acc => {
+        res.json(acc)
+    })
+    .catch(err => {
+        res.json(`${err}`)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Accounts.delete(req.params.id)
+    .then(() => {
+        res.json('deleted')
+    })
+    .catch(err =>{
+        res.json(`${err}`)
+    })
+})
+
+module.exports = router
